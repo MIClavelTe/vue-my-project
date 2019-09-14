cd4cad69061eb7d0cada4251009ff1f745600aa4
Maria Isabelle Te <mariaisabellete@Marias-MacBook-Pro.local>
2019-09-14 11:07:55 -0700

    list rendering

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,6 +8,14 @@
       <h1>{{ product }}</h1>
       <p v-if="inStock">In Stock</p>
       <p v-else>Out of Stock</p>
+
+      <ul>
+        <li v-for="(detail, index) in details" :key="index">{{ detail }}</li>
+      </ul>
+
+      <div v-for="variant in variants" :key="variant.varientsId">
+        <p>{{ variant.variantColor }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -18,7 +26,18 @@ export default {
     return {
       product: 'Socks',
       image: '',
-      inStock: true
+      inStock: true,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: 'green'
+        },
+        {
+          variantId: 2235,
+          variantColor: 'blue'
+        }
+      ]
     }
   }
 }
